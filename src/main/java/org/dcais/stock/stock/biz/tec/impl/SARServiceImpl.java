835a342c07f6b39747f5e42ddf650d2d28159a95
dcais <dyingjie_cai@163.com>
2019-09-28 13:13:35 +0800

    commit

--- src/main/java/org/dcais/stock/stock/biz/tec/impl/SARServiceImpl.java
+++ src/main/java/org/dcais/stock/stock/biz/tec/impl/SARServiceImpl.java
@@ -31,34 +31,6 @@ public class SARServiceImpl extends BaseTaService implements SARService {
   @Autowired
   private XTecSARDao xTecSARDao;
 
-  public void calc(DataFrame df, MAType maType,Integer period){
-
-    CoreAnnotated coreAnnotated = new CoreAnnotated();
-
-    double[] arrClose =  new double[df.length()];
-
-    List<BigDecimal> closes =  df.col("close");
-    for(int i = 0 ; i < closes.size() ; i++ ){
-      arrClose[i] = closes.get(i).doubleValue();
-    }
-
-    MInteger outBegIdx = new MInteger();
-    MInteger outNBElement = new MInteger();
-    double[] outReal = new double[arrClose.length];
-
-
-    List<BigDecimal> ma = new ArrayList<>();
-    if( period <= arrClose.length){
-      coreAnnotated.movingAverage(0, arrClose.length-1, arrClose, period , MAType.Sma, outBegIdx, outNBElement,outReal );
-      ma = convertOutRealToList(outReal,outBegIdx.value,outNBElement.value);
-    }else {
-      for(int i = 0 ; i < arrClose.length ; i++){
-        ma.add(null);
-      }
-    }
-
-    df.add(getTecName(maType,period),ma);
-  }
   public String getTecName(){
     return "sar";
   }
@@ -93,7 +65,7 @@ public class SARServiceImpl extends BaseTaService implements SARService {
     double[] outReal = new double[arrHigh.length];
 
     CoreAnnotated coreAnnotated = new CoreAnnotated();
-    coreAnnotated.sar(0, arrHigh.length-1, arrHigh,arrLow , 0.02d, 0.2d, outBegIdx,outNBElement, outReal );
+    coreAnnotated.sar(0, arrHigh.length-1, arrHigh,arrLow , 0.1, 2d, outBegIdx,outNBElement, outReal );
 
     List<BigDecimal> sars = convertOutRealToList(outReal,outBegIdx.value,outNBElement.value);
     df.add(getTecName(),sars);
